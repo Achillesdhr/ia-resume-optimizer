29ba290aceaf9b56079391b767e9f0a551488636
Gabriel Achilles <g.achilles2007@gmail.com>
2025-07-23 16:26:01 -0300

    Update config.py
    remoção keys

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ class Config:
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
     
     # Configurações da API JSearch
-    JSEARCH_URL = 'https://jsearch.p.rapidapi.com/search'
-    RAPIDAPI_KEY = 'b680b398d6mshc589a9ee9b324fcp175309jsn34141ab70dd5'
-    RAPIDAPI_HOST = 'jsearch.p.rapidapi.com'
+    JSEARCH_URL = 
+    RAPIDAPI_KEY = 
+    RAPIDAPI_HOST = 
     
     # Configurações da API Google Gemini
     GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
